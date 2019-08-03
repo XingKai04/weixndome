43a59c4fe07bfe56d1a70c806e2f3cfee4f43385
XingKai04 <HappkaiYY@163.com>
2019-08-03 21:45:36 +0800

    首页轮播图dome加强

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div @click="clickHandle">
-    <swiper utplay >
-    <swiper-item>
+    <swiper indicator-dots autoplay interval='3000 ' circular >
+    <swiper-item >
       <a href="">
         <img src="/static/uploads/banner1.png" alt="">
       </a>
