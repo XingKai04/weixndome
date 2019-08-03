7d75e6ba9b83314f9a60ded81bde712132b955c7
XingKai04 <HappkaiYY@163.com>
2019-08-03 22:56:27 +0800

    搜索框dome

--- src/components/search.vue
+++ src/components/search.vue
@@ -1,7 +1,8 @@
 <template>
     <div class="search">
         <view class="search-box" > 
-            <input type="text" placeholder="搜素内容">
+            <input type="text">
+            <span></span>
         </view>
         <!-- <a href="">取消</a> -->
     </div>
@@ -28,6 +29,23 @@ export default {
     height: 60rpx;
     padding: 0 30rpx 0 40rpx;
     background-color: aliceblue;
+    border-radius: 8rpx;
+    position: relative;
     
+}
+.search-box::after {
+    content:'搜索';
+    display: block;
+    position: absolute;
+    width: 96rpx;
+    height: 50rpx;
+    transform: translate(-50%,-50%);
+
+    left: 50%;
+    top: 50%;
+
+
+}
+input {
 }
 </style>
