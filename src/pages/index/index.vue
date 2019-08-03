2433d8e90d459366e2dd87fa66e70110a8d89d88
XingKai04 <HappkaiYY@163.com>
2019-08-03 21:36:23 +0800

    首页轮播图dome

---
+++ src/pages/index/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <div @click="clickHandle">
+    <swiper utplay >
+    <swiper-item>
+      <a href="">
+        <img src="/static/uploads/banner1.png" alt="">
+      </a>
+    </swiper-item>
+    <swiper-item>
+      <a href="">
+        <img src="/static/uploads/banner2.png" alt="">
+      </a>
+    </swiper-item>
+    <swiper-item>
+      <a href="">
+        <img src="/static/uploads/banner3.png" alt="">
+      </a>
+    </swiper-item>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+
+
+</script>
+
+<style scoped>
+swiper {
+  height: 340rpx;
+}
+swiper img{
+  width: 750rpx;
+  height: 340rpx;
+}
+
+</style>
