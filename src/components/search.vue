2ea16f775177c11c84ad79cc1ddecd4d5c1cd4a8
XingKai04 <HappkaiYY@163.com>
2019-08-03 21:58:17 +0800

    首页功能 vue实例组件导入测试

---
+++ src/components/search.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="">
+       组件1dome
+    </div>
+</template>
+
+<script>
+export default {
+    data () {
+
+        return{
+
+        }
+    }
+}
+</script>
+<style>
+
+</style>
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,40 +1,50 @@
 <template>
   <div @click="clickHandle">
-    <swiper indicator-dots autoplay interval='3000 ' circular >
-    <swiper-item >
-      <a href="">
-        <img src="/static/uploads/banner1.png" alt="">
-      </a>
-    </swiper-item>
-    <swiper-item>
-      <a href="">
-        <img src="/static/uploads/banner2.png" alt="">
-      </a>
-    </swiper-item>
-    <swiper-item>
-      <a href="">
-        <img src="/static/uploads/banner3.png" alt="">
-      </a>
-    </swiper-item>
-    </swiper>
+    <view>
+      <!-- 伦播图 -->
+      <swiper indicator-dots autoplay interval="3000 " circular>
+        <swiper-item>
+          <a href>
+            <img src="/static/uploads/banner1.png" alt />
+          </a>
+        </swiper-item>
+        <swiper-item>
+          <a href>
+            <img src="/static/uploads/banner2.png" alt />
+          </a>
+        </swiper-item>
+        <swiper-item>
+          <a href>
+            <img src="/static/uploads/banner3.png" alt />
+          </a>
+        </swiper-item>
+      </swiper>
+    </view>
+    <search/>
+
   </div>
 </template>
 
 <script>
+import search from "@/components/search.vue";
 export default {
+    components: {
+      search
+    },
+  data () {
+    return {
 
-}
-
-
+    }
+  }
+};
 </script>
 
 <style scoped>
 swiper {
   height: 340rpx;
 }
-swiper img{
+swiper img {
   width: 750rpx;
   height: 340rpx;
 }
-
 </style>
